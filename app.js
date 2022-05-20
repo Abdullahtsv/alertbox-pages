89d83a036d8e6e786e8fc158cf33abeaaeaf1e90
Abdullah Bhatti <99602533+Abdullahtsv@users.noreply.github.com>
2022-05-20 16:39:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var userName = prompt ("Please enter User_Name");
+var myName = "Abdullah Bhatti";
+var message = "Hello World";
+
+
+
+alert(myName)
+alert("17 years old.");
+alert("SSUET");
+alert("Mobile & Cloud Computing - Module A");
+alert("PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P");
+
+
+
+
+
+
+
+if (userName === userName){
+    alert("welcome" + "\n" + userName + "\n" + "Please enter your password!");    
+}
+else{
+alert("Error! Please enter a valid User_Name.");
+}
+var password = prompt ("Please enter your password" + " " + "Hint (Pakistani)");
+
+if (password === "Pakistani"){
+    alert(welcome + "\n" + happycode);    
+}
+else{
+    alert("Error! Please enter a valid password.");
+    }
